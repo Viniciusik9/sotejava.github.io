9e54f3ff043dab8fb1576550bba5a28707aaad57
Viniciusik9 <138507437+Viniciusik9@users.noreply.github.com>
2024-09-13 09:24:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ varH1=document.querySelector('h1');
 varH1.innerHTML='secret_number';
 
 varP=document.querySelector('p');
-varP.innerHTML='type a number between 1 and 10';
+varP.innerHTML='type a number between 1 and 100';
 let secret_number= Math.floor((Math.random()*10)+1);
 function verificarChute(){
 
